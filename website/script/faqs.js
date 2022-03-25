e9cf9beff66764373685e98261aa14551a451025
s45282079 <99326432+s45282079@users.noreply.github.com>
2022-03-25 15:48:27 +0000

    Added FAQ script

---
+++ website/script/faqs.js
@@ -0,0 +1,80 @@
+function hide1() {
+  var x = document.getElementById("hide1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide2() {
+  var x = document.getElementById("hide2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide3() {
+  var x = document.getElementById("hide3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide4() {
+  var x = document.getElementById("hide4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide5() {
+  var x = document.getElementById("hide5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide6() {
+  var x = document.getElementById("hide6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide7() {
+  var x = document.getElementById("hide7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide8() {
+  var x = document.getElementById("hide8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide9() {
+  var x = document.getElementById("hide9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hide10() {
+  var x = document.getElementById("hide10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
